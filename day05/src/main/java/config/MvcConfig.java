b8d41de98c750a7a6664892a82a9d406841bbe51
bebeaubn <yebin0213@naver.com>
2023-11-06 14:36:36 +0900

    수업 소스

--- day05/src/main/java/config/MvcConfig.java
+++ day05/src/main/java/config/MvcConfig.java
@@ -25,6 +25,9 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/")
                 .setViewName("main/index");
+
+        registry.addViewController("/mypage/**")
+                .setViewName("member/mypage");
     }
 
     @Autowired
